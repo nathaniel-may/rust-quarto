06c2ce7941a4ffa193b852ef344d14270bcc4ee0
Nathaniel May <nate.may@fauna.com>
2021-01-21 16:42:30 -0500

    added basic structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[derive(Copy, Clone)]
+enum Color {
+    White,
+    Black,
+}
+
+#[derive(Copy, Clone)]
+enum Height {
+    Tall,
+    Short,
+}
+
+#[derive(Copy, Clone)]
+enum Shape {
+    Round,
+    Square,
+}
+
+#[derive(Copy, Clone)]
+enum Top {
+    Flat,
+    Hole,
+}
+
+#[derive(Copy, Clone)]
+struct Piece {
+    color: Color,
+    height: Height,
+    shape: Shape,
+    top: Top,
+}
+
+#[derive(Copy, Clone)]
+struct Board {
+    pieces: [[Option<Piece>; 4]; 4],
+}
+
+fn new_board() -> Board {
+    Board {
+        pieces: [[None; 4]; 4],
+    }
+}
+
+#[derive(Copy, Clone)]
+enum H {
+    H1,
+    H2,
+    H3,
+    H4,
+}
+
+#[derive(Copy, Clone)]
+enum V {
+    V1,
+    V2,
+    V3,
+    V4
+}
+
+fn h_to_int(h: H) -> usize {
+    match h {
+        H::H1 => 1,
+        H::H2 => 2,
+        H::H3 => 3,
+        H::H4 => 4,
+    }
+}
+
+fn v_to_int(v: V) -> usize {
+    match v {
+        V::V1 => 1,
+        V::V2 => 2,
+        V::V3 => 3,
+        V::V4 => 4,
+    }
+}
+
+fn get_piece(b: Board, square: (H, V)) -> Option<Piece> {
+    b.pieces[h_to_int(square.0)][v_to_int(square.1)]
+}
