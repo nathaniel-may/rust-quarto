5ee30d22e6b316da5a565178dc98c4ca7194a26e
Nathaniel May <nate.may@fauna.com>
2021-01-22 12:06:20 -0500

    moved board into separate file

---
+++ src/board.rs
@@ -0,0 +1,47 @@
+use crate::piece::Piece;
+
+#[derive(Copy, Clone)]
+pub struct Board {
+    pieces: [[Option<Piece>; 4]; 4],
+}
+
+impl Board {
+    fn place_piece(&mut self, square: (Idx, Idx), p: Piece) -> Option<Board> {
+        match self.get_piece(square) {
+            None => {
+                self.pieces[square.0.to_i()][square.1.to_i()] = Some(p);
+                Some(*self)
+            }
+            Some(_) => None
+        }
+    }
+
+    fn get_piece(&self, square: (Idx, Idx)) -> Option<Piece> {
+        self.pieces[square.0.to_i()][square.1.to_i()]
+    }
+}
+
+pub fn new_board() -> Board {
+    Board {
+        pieces: [[None; 4]; 4],
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Idx {
+    I1,
+    I2,
+    I3,
+    I4,
+}
+
+impl Idx {
+    fn to_i(&self) -> usize {
+        match *self {
+            Idx::I1 => 1,
+            Idx::I2 => 2,
+            Idx::I3 => 3,
+            Idx::I4 => 4,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,8 @@
 mod piece;
+mod board;
 use crate::piece::Piece;
+use crate::board::Board;
 
 fn main() {
     println!("Hello, world!");
-}
-
-#[derive(Copy, Clone)]
-struct Board {
-    pieces: [[Option<Piece>; 4]; 4],
-}
-
-impl Board {
-    fn place_piece(&mut self, square: (Idx, Idx), p: Piece) -> Option<Board> {
-        match self.get_piece(square) {
-            None => {
-                self.pieces[square.0.to_i()][square.1.to_i()] = Some(p);
-                Some(*self)
-            }
-            Some(_) => None
-        }
-    }
-
-    fn get_piece(&self, square: (Idx, Idx)) -> Option<Piece> {
-        self.pieces[square.0.to_i()][square.1.to_i()]
-    }
-}
-
-fn new_board() -> Board {
-    Board {
-        pieces: [[None; 4]; 4],
-    }
-}
-
-#[derive(Copy, Clone)]
-enum Idx {
-    I1,
-    I2,
-    I3,
-    I4,
-}
-
-impl Idx {
-    fn to_i(&self) -> usize {
-        match *self {
-            Idx::I1 => 1,
-            Idx::I2 => 2,
-            Idx::I3 => 3,
-            Idx::I4 => 4,
-        }
-    }
 }
