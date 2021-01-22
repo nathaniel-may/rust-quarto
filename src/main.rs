3c2068632cf1b7e28ce3bd37a1c00b17b79f278b
Nathaniel May <nate.may@fauna.com>
2021-01-22 11:45:43 -0500

    simplified indexing

--- src/main.rs
+++ src/main.rs
@@ -40,14 +40,18 @@ struct Board {
 }
 
 impl Board {
-    fn place_piece(&mut self, square: (H, V), p: Piece) -> Option<Board> {
-        match get_piece(*self, square) {
+    fn place_piece(&mut self, square: (Idx, Idx), p: Piece) -> Option<Board> {
+        match self.get_piece(square) {
             None => {
-                self.pieces[h_to_i(square.0)][v_to_i(square.1)] = Some(p);
+                self.pieces[square.0.to_i()][square.1.to_i()] = Some(p);
                 Some(*self)
             }
             Some(_) => None
         }
+    }
+
+    fn get_piece(&self, square: (Idx, Idx)) -> Option<Piece> {
+        self.pieces[square.0.to_i()][square.1.to_i()]
     }
 }
 
@@ -58,39 +62,20 @@ fn new_board() -> Board {
 }
 
 #[derive(Copy, Clone)]
-enum H {
-    H1,
-    H2,
-    H3,
-    H4,
+enum Idx {
+    I1,
+    I2,
+    I3,
+    I4,
 }
 
-#[derive(Copy, Clone)]
-enum V {
-    V1,
-    V2,
-    V3,
-    V4
-}
-
-fn h_to_i(h: H) -> usize {
-    match h {
-        H::H1 => 1,
-        H::H2 => 2,
-        H::H3 => 3,
-        H::H4 => 4,
+impl Idx {
+    fn to_i(&self) -> usize {
+        match *self {
+            Idx::I1 => 1,
+            Idx::I2 => 2,
+            Idx::I3 => 3,
+            Idx::I4 => 4,
+        }
     }
-}
-
-fn v_to_i(v: V) -> usize {
-    match v {
-        V::V1 => 1,
-        V::V2 => 2,
-        V::V3 => 3,
-        V::V4 => 4,
-    }
-}
-
-fn get_piece(b: Board, square: (H, V)) -> Option<Piece> {
-    b.pieces[h_to_i(square.0)][v_to_i(square.1)]
 }
