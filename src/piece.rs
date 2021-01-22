75e35d3eab8f8fd2322f70229d7744f48576fc1b
Nathaniel May <nate.may@fauna.com>
2021-01-22 12:18:39 -0500

    adding fmt for Piece

--- src/piece.rs
+++ src/piece.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Copy, Clone)]
 pub enum Color {
     White,
@@ -28,4 +30,27 @@ pub struct Piece {
     height: Height,
     shape: Shape,
     top: Top,
+}
+
+impl fmt::Display for Piece {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut s: String = "".to_owned();
+        match self.color {
+            Color::White => s = s + "W",
+            Color::Black => s = s + "B",
+        }
+        match self.height {
+            Height::Tall => s = s + "T",
+            Height::Short => s = s + "S",
+        }
+        match self.shape {
+            Shape::Round => s = s + "R",
+            Shape::Square => s = s + "Q",
+        }
+        match self.top {
+            Top::Flat => s = s + "F",
+            Top::Hole => s = s + "H",
+        }
+        write!(f, "{}", s)
+    }
 }
